7b7f58fd208c43ef23c654161da2b3d3298e2c96
bayzed08 <gbir.islam@gmail.com>
2021-10-05 23:48:15 +0600

    added db

--- custom/js/Supplier_Info.js
+++ custom/js/Supplier_Info.js
@@ -1,11 +1,12 @@
 var supplierTankTable;
 $(document).ready(
-    function() {
-        supplierTankTable=$("#supplierTankTable").DataTable({
-           "ajax":"supplierlistRetrieve.php",
-           "order":[]
-       });
+    // function() {
+    //     supplierTankTable=$("#supplierTankTable").DataTable({
+    //        "ajax":"supplierlistRetrieve.php",
+    //        "order":[]
+    //    });
     $("#supplierinfo").click(function () {
         $("#supplierlistshow").slideToggle(700);
-    });
-    });
+         })
+        //;}
+        );
